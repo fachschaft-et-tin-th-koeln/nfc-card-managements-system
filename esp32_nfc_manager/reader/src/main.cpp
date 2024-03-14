d24face716cbfe262661da41a61f349995d92211
Leo <k.leonard2@t-online.de>
2024-03-14 19:50:49 +0100

    !

--- esp32_nfc_manager/reader/src/main.cpp
+++ esp32_nfc_manager/reader/src/main.cpp
@@ -1,18 +1,97 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SS_PIN 5
+#define RST_PIN 17
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+
+MFRC522::MIFARE_Key key;
+
+// Init array that will store new NUID
+byte nuidPICC[4];
+
+bool flag = false;
+
+void setup()
+{
+  Wire.begin(15, 2); // SDA, SCL
+  lcd.clear();
+  lcd.init(); // initialize the lcd
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("please present");
+  lcd.setCursor(0, 1);
+  lcd.print("your card");
+
+  Serial.begin(115200);
+  SPI.begin();     // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522
+
+  for (byte i = 0; i < 6; i++)
+  {
+    key.keyByte[i] = 0xFF;
+  }
+
+  Serial.println(F("This code scan the MIFARE Classsic NUID."));
+  Serial.print(F("Using the following key:"));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  if (flag)
+  {
+    delay(4000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("please present");
+    lcd.setCursor(0, 1);
+    lcd.print("your card");
+    flag = false;
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if (!rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if (!rfid.PICC_ReadCardSerial())
+    return;
+
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+  {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  for (byte i = 0; i < 4; i++)
+  {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("UID: " + String(nuidPICC[0], HEX) + " " + String(nuidPICC[1], HEX) + " " + String(nuidPICC[2], HEX) + " " + String(nuidPICC[3], HEX));
+  flag = true;
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
 }
